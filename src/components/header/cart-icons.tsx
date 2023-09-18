4a2f66248c5a0e46374011ea5d4225fe5e80ea68
Alexis Sniffer <alexis.sniffer@gmail.com>
2023-09-18 00:34:23 -0500

    cart drawer

--- src/components/header/cart-icons.tsx
+++ src/components/header/cart-icons.tsx
@@ -5,24 +5,39 @@ import {
   SearchOutlined,
   ShoppingOutlined,
 } from '@ant-design/icons'
-import { Badge, Button, Col, Row } from 'antd'
+import { Badge, Col, Row } from 'antd'
+import Link from 'next/link'
+import { useState } from 'react'
+import CartDrawer from './cart-drawer'
 
 export default function CartIcons() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <Row gutter={{ xs: 24, sm: 24, md: 24 }}>
-      <Col flex={'0 0 auto'} xs={0} sm={24} lg={0}>
-        <SearchOutlined style={{ fontSize: '1.7rem' }} rev={undefined} />
-      </Col>
-      <Col>
-        <Badge count={4}>
-          <HeartOutlined style={{ fontSize: '1.7rem' }} rev={undefined} />
-        </Badge>
-      </Col>
-      <Col>
-        <Badge count={4}>
-          <ShoppingOutlined style={{ fontSize: '1.7rem' }} rev={undefined} />
-        </Badge>
-      </Col>
-    </Row>
+    <>
+      <Row gutter={{ xs: 24, sm: 24, md: 24 }}>
+        <Col flex={'0 0 auto'} xs={0} sm={24} lg={0}>
+          <SearchOutlined style={{ fontSize: '1.7rem' }} rev={undefined} />
+        </Col>
+        <Col>
+          <Link href={'/wishlist'}>
+            <Badge count={4}>
+              <HeartOutlined style={{ fontSize: '1.7rem' }} rev={undefined} />
+            </Badge>
+          </Link>
+        </Col>
+        <Col>
+          <Badge count={4}>
+            <ShoppingOutlined
+              style={{ fontSize: '1.7rem' }}
+              rev={undefined}
+              onClick={() => setOpen(!open)}
+            />
+          </Badge>
+        </Col>
+      </Row>
+
+      <CartDrawer isOpen={open} onClose={() => setOpen(!open)} />
+    </>
   )
 }
